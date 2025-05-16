563d7fa2ebb23cd4429813940e8032b9a973f428
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-16 11:29:14 +0000

    Update app.py

--- pm25_monitoring_app_full/app.py
+++ pm25_monitoring_app_full/app.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="PM₂.₅ Monitoring App", layout="wide")
 
 import streamlit_authenticator as stauth
 from modules.user_management import login, logout_button
-from pages import main, data_entry, edit_records, pm_calculation, admin_tools
+from pages import main, data_entry, edit_records, pm_calculation, admin
 
 
 # === Session Login ===

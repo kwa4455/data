310b86e8828a4f26afcc5068ff8d20c5343584a1
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-21 21:18:05 +0000

    Update constants.py

--- pm25_monitoring_app_full/constants.py
+++ pm25_monitoring_app_full/constants.py
@@ -2,3 +2,6 @@ SPREADSHEET_ID = "1jCV-IqALZz7wKqjqc5ISrkA_dv35mX1ZowNqwFHf6mk"
 MAIN_SHEET = 'Observations'
 MERGED_SHEET = 'Merged Records'
 CALC_SHEET = "PM Calculations"
+REG_REQUESTS_SHEET = "Registration Requests"
+USERS_SHEET = "Users"
+LOG_SHEET = "Registration Log"

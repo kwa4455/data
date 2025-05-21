d1df4200f3bbbc0f745f090d31fade945e62ac0d
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-21 22:34:04 +0000

    Update edit_data_entry_form.py

--- pm25_monitoring_app_full/components/edit_data_entry_form.py
+++ pm25_monitoring_app_full/components/edit_data_entry_form.py
@@ -19,9 +19,11 @@ from modules.authentication import require_role
 
 
 def show():
+    require_role(["admin", "collector","editor"])
     st.subheader("📥 Edit Data Entry Form")
+    st.write("This is where the data entry form would go.")
     
-require_role(["admin", "collector","editor"])
+
 # Inject Google Fonts and custom CSS for glassmorphism and font clarity
 st.markdown("""
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">

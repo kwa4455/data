0f8017fafdf923e22bc88f3f04d42e41e36ac2c2
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-21 20:14:43 +0000

    Update user_utils.py

--- pm25_monitoring_app_full/modules/user_utils.py
+++ pm25_monitoring_app_full/modules/user_utils.py
@@ -1,19 +1,20 @@
-import streamlit as st
 import streamlit_authenticator as stauth
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import json
-# === Google Sheets Auth ===
-creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-]
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-client = gspread.authorize(creds)
-spreadsheet = client.open_by_key(st.secrets["SPREADSHEET_ID"])
+from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
+
+def get_gspread_client():
+    creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    return gspread.authorize(creds)
+)
 
 
 def ensure_users_sheet(spreadsheet):

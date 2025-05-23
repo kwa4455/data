d323b4ddb74e1764997d43d30baa0aa31f398566
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-23 08:47:30 +0000

    Update data_entry_form.py

--- pm25_monitoring_app_full/components/data_entry_form.py
+++ pm25_monitoring_app_full/components/data_entry_form.py
@@ -181,9 +181,11 @@ def show():
         except Exception as e:
             st.warning(f"⚠ Could not load Submitted Monitoring Records: {e}")
 
+     # --- Footer ---
     st.markdown("""
         <hr style="margin-top: 40px; margin-bottom:10px">
         <div style='text-align: center; color: grey; font-size: 0.9em;'>
-            © 2025 EPA Ghana · Developed by Clement Mensah Ackaah 🦺 · Built with 😍 using Streamlit
+            © 2025 EPA Ghana · Developed by Clement Mensah Ackaah 🦺 · Built with 😍 using Streamlit | 
+            <a href="mailto:clement.ackaah@epa.gov.gh">Contact Support</a>
         </div>
     """, unsafe_allow_html=True)

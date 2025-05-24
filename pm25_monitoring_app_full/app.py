c7d3a1b86d348820f0d37d37d1bedb1ed5e3b256
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-24 09:58:56 +0000

    Update app.py

--- pm25_monitoring_app_full/app.py
+++ pm25_monitoring_app_full/app.py
@@ -98,15 +98,7 @@ with st.sidebar:
 # Call theming setup
 apply_custom_theme()
 
-# Icon per theme
-theme_icon_map = {
-    "Light": "🌞",
-    "Dark": "🌙",
-    "Blue": "💧",
-    "Green": "🌿",
-    "Purple": "🔮"
-}
-icon = theme_icon_map[st.session_state.theme]
+
 
 # Content with animation
 st.markdown('<div class="slide-in">', unsafe_allow_html=True)

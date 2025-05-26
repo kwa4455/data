1a875b13aa70774c791422913fc732158c2cd695
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-26 15:28:03 +0000

    Update app.py

--- pm25_monitoring_app_full/app.py
+++ pm25_monitoring_app_full/app.py
@@ -66,7 +66,7 @@ st.info(f"👤 Logged in as: **{username}** (Role: {role})")
 # === Load Data Once ===
 if "df" not in st.session_state:
     with st.spinner("🔄 Loading data..."):
-        st.session_state.df = load_data_from_sheet_cached(_sheet=sheet)
+        st.session_state.df = load_data_from_sheet_cached(sheet)
         st.session_state.sheet = sheet
         st.session_state.spreadsheet = spreadsheet
 

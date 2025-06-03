7e8d2eef4732670ca4c116052dfd316e0fdf116c
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-06-03 14:55:23 +0000

    Update apartment.py

--- pm25_monitoring_app_full/components/apartment.py
+++ pm25_monitoring_app_full/components/apartment.py
@@ -84,14 +84,7 @@ def show():
         <p><em>{text['note'][lang]}</em></p>
     """, unsafe_allow_html=True)
 
-    # Chat input
-    st.markdown("---")
-    prompt = st.chat_input("Say something and/or attach an image", accept_file=True, file_type=["jpg", "jpeg", "png"])
-    if prompt and prompt.text:
-        st.markdown(prompt.text)
-    if prompt and prompt["files"]:
-        st.image(prompt["files"][0])
-
+   
     # Feedback
     sentiment_mapping = ["one", "two", "three", "four", "five"]
     selected = st.feedback("stars")

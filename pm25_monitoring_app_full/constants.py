028811b99edcf0441fb76a853959615a976db54b
Clement Menash Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-06-05 20:54:53 +0000

    Update constants.py

--- pm25_monitoring_app_full/constants.py
+++ pm25_monitoring_app_full/constants.py
@@ -1,4 +1,4 @@
-SPREADSHEET_ID = "1jCV-IqALZz7wKqjqc5ISrkA_dv35mX1ZowNqwFHf6mk"
+SPREADSHEET_ID = "1ZryycIkcgBwdXGR-VRaEqwb3D1YE47CCt0kPow08IEA"
 MAIN_SHEET = 'Observations'
 MERGED_SHEET = 'Merged Records'
 CALC_SHEET = "PM Calculations"
